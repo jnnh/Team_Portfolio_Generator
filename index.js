fa29c7d708fd9834e59dc48918bb689c6bfa21a7
jnnh <jenniferhorvath@cmail.carleton.ca>
2022-02-06 19:37:06 -0500

    manager prompts working

--- index.js
+++ index.js
@@ -1,4 +1,85 @@
-const App = require('./lib/App');
+const inquirer = require('inquirer');
+// const Engineer = require('./Engineer');
+// const Intern = require('./Intern');
 
 
-new App.startApp();
+const createEngineer = () => {
+    console.log ("engineer");
+}
+const createIntern = () => {
+    console.log ("intern");
+}
+
+const startApp = () =>{
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the Manager's Name (Required)?",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    }
+                    else {
+                        console.log("Please enter the Manager's name");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'number',
+                name: 'id',
+                message: "What is the Manager's Id Number"
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is the Manager's email?"
+            },
+            {
+                type: 'input',
+                name: 'office',
+                message: "What is the Manager's office number?"
+            }
+        ])}
+const addEmployee= managerData => {
+        if (!managerData.employees){
+            managerData.employees = [];
+        }
+        return inquirer.prompt([
+            {
+                type: 'confirm',
+                name: 'addEmployee',
+                message: 'Would you like to add an additional employee?',
+                default: false
+            },
+            {
+                type: 'list',
+                name: 'roles',
+                message: 'What type of employee would you like to add?',
+                choices: ['Engineer', 'Intern', 'Finish Building Team'],
+                when: ({addEmployee})=>{
+                    if (addEmployee){
+                        return true;
+                    } 
+                    else {
+                        return false;
+                    }
+                }
+            }
+        ])
+    .then(employeeData => {
+        if (employeeData.roles === 'Engineer'){
+            createEngineer ();
+        }
+        else if (employeeData.roles === 'Intern'){
+            createIntern ();
+        }
+        else{
+            return employeeData;
+        }
+    });
+};
+
+startApp()
+    .then(addEmployee);
